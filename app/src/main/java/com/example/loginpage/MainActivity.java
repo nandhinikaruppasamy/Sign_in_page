94194f3a577c0afbfb58980af5c3721b19f2c519
Nandhini <21cs110@kpriet.ac.in>
2023-08-15 20:42:40 +0530

    initial commit

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button butgoto;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        butgoto = findViewById(R.id.but); // Make sure the ID matches the one in your layout XML
+        butgoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
